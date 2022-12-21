38bb2a3c218f76ea17a92e3c2341a60455ff6167
Salvatore Capano <capano_salvatore@libero.it>
2022-12-21 22:10:17 +0100

    Bonus 1 completato

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
-// Scrivi un programma che stampi in console i numeri da 1 a 100, 
-// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. 
-// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+const body = document.querySelector('body');
+const newUl = document.createElement("ul")
+
+body.append(newUl);
 
 for (let i=1; i<101 ; i++) {
 
+    const newLi = document.createElement("li");
+
     if (((i % 3) == 0) && ((i % 5) == 0)) {
         console.log("FizzBuzz");
+        newLi.innerHTML = "FizzBuzz";
     }
     else if ((i % 3) == 0) {
         console.log("Fizz");
+        newLi.innerHTML = "Fizz";
     }
     else if ((i % 5) == 0){
         console.log("Buzz");
+        newLi.innerHTML = "Buzz";
     }
     else {
         console.log(i);
+        newLi.innerHTML = i;
     }
+
+    newUl.append(newLi);
 }
