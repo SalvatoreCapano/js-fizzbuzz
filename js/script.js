703b81d9ca53d4721b1b766e7e45300fe8fc2c6f
Salvatore Capano <capano_salvatore@libero.it>
2022-12-21 22:29:11 +0100

    Bonus 2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 const body = document.querySelector('body');
+
+const newContainer = document.createElement("div")
 const newUl = document.createElement("ul")
 
-body.append(newUl);
+body.append(newContainer);
+newContainer.append(newUl);
+
+newContainer.classList.add("container");
+newUl.classList.add("myList");
 
 for (let i=1; i<101 ; i++) {
 
     const newLi = document.createElement("li");
+    newLi.classList.add("myListElement");
 
     if (((i % 3) == 0) && ((i % 5) == 0)) {
         console.log("FizzBuzz");
         newLi.innerHTML = "FizzBuzz";
+        newLi.classList.add("myFizzBuzz");
     }
     else if ((i % 3) == 0) {
         console.log("Fizz");
         newLi.innerHTML = "Fizz";
+        newLi.classList.add("myFizz");
     }
     else if ((i % 5) == 0){
         console.log("Buzz");
         newLi.innerHTML = "Buzz";
+        newLi.classList.add("myBuzz");
     }
     else {
         console.log(i);
